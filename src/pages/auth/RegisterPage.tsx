79fe2479b21dde7c6138bc94c0ab6f2803beab7d
FARAMARZ <faramarz@FARAMARZs-MacBook-Pro.local>
2025-05-31 23:39:56 +0330

    clean code

--- src/pages/auth/RegisterPage.tsx
+++ src/pages/auth/RegisterPage.tsx
@@ -14,7 +14,7 @@ const registerSchema = z
     password: z.string().min(6, 'Password must be at least 6 characters'),
     confirmPassword: z.string().min(1, 'Please confirm your password'),
   })
-  .refine((data) => data.password === data.confirmPassword, {
+  .refine((data:any) => data.password === data.confirmPassword, {
     message: "Passwords don't match",
     path: ['confirmPassword'],
   });
